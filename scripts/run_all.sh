4182366c2770788ca6b34237d65dd935ff8782a1
Florian Kalinke <flops.ka@gmail.com>
2024-05-07 10:36:13 +0200

    add FSSD-rand

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-python scripts/run_gof_experiment.py laplace
-python scripts/run_gof_experiment.py student-t
-python scripts/run_gof_experiment.py rbm
-python scripts/run_gof_experiment.py null
-python scripts/run_speed_experiment.py
+python scripts/run_gof_experiment.py laplace -o full_results
+python scripts/run_gof_experiment.py student-t -o full_results
+python scripts/run_gof_experiment.py rbm -o full_results
+python scripts/run_speed_experiment.py -o full_results
